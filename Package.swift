89e4053dc440e2c0e531fab84b303b0025d7c77d
kPherox <admin@mail.kr-kp.com>
2018-03-10 03:51:58 +0900

    Change SwiftPM version 3.1 -> 4.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,16 @@
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OptionKit",
-    targets: [],
-    dependencies: []
+    targets: [
+        .target(
+            name: "OptionKit",
+            path: "Sources"),
+        .testTarget(
+            name: "OptionKitTests",
+            dependencies: ["OptionKit"]),
+    ]
 )
